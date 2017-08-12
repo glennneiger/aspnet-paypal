e4e091d9571977cce4a8d7332619c06cd1091ea3
metsephiri <metse.phiri@yahoo.com>
2017-08-12 11:22:39 +0200

    initial commit

--- ASPNETPayPal/App_Start/BundleConfig.cs
+++ ASPNETPayPal/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace ASPNETPayPal
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css"));
         }
     }
 }
---
+++ ASPNETPayPal/Services/PayPalPaymentService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PayPal.Api;
+
+namespace ASPNETPayPal.Services
+{
+    public class PayPalPaymentService : IPayPalPaymentService
+    {
+        public Payment CreatePayment(string url, string intent)
+        {
+
+            // Using apiContext to authenticate call.
+            var apiContext = GetApiContext();
+
+            var payment = new Payment
+            {
+                intent = "sale",
+                payer = new Payer
+                {
+                    payment_method = "paypal"
+                }, 
+                transactions = GetTransactions()
+            };
+
+            // Created payment.
+            var created = payment.Create(apiContext);
+
+            return created;
+        }
+
+        public Payment ExecutePayment(string paymentId, string payerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public APIContext GetApiContext()
+        {
+            // Fetch configuration values.
+            var configuration = ConfigManager.Instance.GetProperties();
+
+            var token = new OAuthTokenCredential(configuration).GetAccessToken();
+
+            return new APIContext(token);
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            // Returning a list of transactions for the payment.
+
+            return new List<Transaction>()
+            {
+                new Transaction
+                {
+                    description = "Random Transaction Description.",
+                    invoice_number = new Guid().ToString(),
+                    amount = new Amount
+                    {
+                        currency = "USD",
+                        total = "50.00",
+                        details = new Details
+                        {
+                            tax = "10",
+                            shipping = "10",
+                            subtotal = "30"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
